ed443fd52a946cd7ec54dbea0e4dbf844e3fdbb8
Sammers21 <titantins@gmail.com>
2024-07-06 02:24:54 +0300

    general improvements

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -5,3 +5,33 @@ script.src = chrome.runtime.getURL('script.js');
 script.onload = function () {
   script.remove();
 };
+
+fetch('https://www.dotabuff.com/heroes/arc-warden', {
+  method: 'GET',
+})
+  .then((response) => {
+    console.log('response', response);
+    return response.text();
+  })
+  .then((data) => {
+    console.log('data', data);
+  });
+
+
+// chrome.storage.local.get('enabled',  async function (result) {  
+//   if (result == undefined) {
+//     // Here you define the logic of your script for the website
+//     console.log('Owl esports does not exist, creating it and enabling it');
+//     const res = chrome.storage.local.set({ enabled: true })
+//     const awaitRes = await res;
+//     console.log('Owl esports enbling result', awaitRes);
+//   } else if (result.enabled) {
+//     console.log('Owl esports enabled, disabling it');
+//     const res = await chrome.storage.local.set({ enabled: false })
+//     console.log('Owl esports disabling result', res);
+//   } else if (!result.enabled) {
+//     console.log('Owl esports not enabled, enabling it');
+//     const res = await chrome.storage.local.set({ enabled: true })
+//     console.log('Owl esports enabling result', res);
+//   }
+// });
--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,1 +1,2 @@
 // Here you define the logic of your script for the website
+console.log('Hello, world from Owl esports!');
